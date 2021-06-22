909fd7e3f56d89e724ae44176594ecb766cc280f
maxsiz <maxsizmobile@gmail.com>
2021-06-23 00:37:05 +0800

    comment max beneficiary tests

--- tests/test_locker_checker_02.py
+++ tests/test_locker_checker_02.py
@@ -30,49 +30,49 @@ def test_deposit_token1(accounts, locker, projecttoken):
 
     logging.info(projecttoken.balanceOf(locker.address))
 
+### This  tests  need for livenet
+
+# def test_deposit_max1(accounts, locker, projecttoken):
+#     userlist = []
+#     shares = []
+#     for i in range(200):
+#         userlist.append(accounts[1])
+#         shares.append(50)
+#     projecttoken.approve(locker.address, projecttoken.balanceOf(accounts[0]), {'from':accounts[0]})
+#     locker.lockTokens(
+#         projecttoken.address,
+#         100e18,
+#         [chain.time() + 1000],
+#         [100e18],
+#         userlist,
+#         shares,
+#         {'from': accounts[0]}
+#     )
+#     logging.info(projecttoken.balanceOf(locker.address))
 
 
-def test_deposit_max1(accounts, locker, projecttoken):
-    userlist = []
-    shares = []
-    for i in range(200):
-        userlist.append(accounts[1])
-        shares.append(50)
-    projecttoken.approve(locker.address, projecttoken.balanceOf(accounts[0]), {'from':accounts[0]})
-    locker.lockTokens(
-        projecttoken.address,
-        100e18,
-        [chain.time() + 1000],
-        [100e18],
-        userlist,
-        shares,
-        {'from': accounts[0]}
-    )
-    logging.info(projecttoken.balanceOf(locker.address))
+# def test_deposit_max2(accounts, locker, projecttoken):
+#     userlist = []
+#     shares = []
+#     for i in range(200):
+#         userlist.append(accounts[1])
+#         shares.append(20)
+#     for j in range(30):
+#         userlist.append(accounts[1])
+#         shares.append(100)
+#     for k in range(8):
+#         userlist.append(accounts[1])
+#         shares.append(375)
 
-
-def test_deposit_max2(accounts, locker, projecttoken):
-    userlist = []
-    shares = []
-    for i in range(200):
-        userlist.append(accounts[1])
-        shares.append(20)
-    for j in range(30):
-        userlist.append(accounts[1])
-        shares.append(100)
-    for k in range(8):
-        userlist.append(accounts[1])
-        shares.append(375)
-
-    projecttoken.approve(locker.address, projecttoken.balanceOf(accounts[0]), {'from':accounts[0]})
-    locker.lockTokens(
-        projecttoken.address,
-        100e18,
-        [chain.time() + 1000],
-        [100e18],
-        userlist,
-        shares,
-        {'from': accounts[0]}
-    )
-    logging.info(projecttoken.balanceOf(locker.address))
+#     projecttoken.approve(locker.address, projecttoken.balanceOf(accounts[0]), {'from':accounts[0]})
+#     locker.lockTokens(
+#         projecttoken.address,
+#         100e18,
+#         [chain.time() + 1000],
+#         [100e18],
+#         userlist,
+#         shares,
+#         {'from': accounts[0]}
+#     )
+#     logging.info(projecttoken.balanceOf(locker.address))
 
