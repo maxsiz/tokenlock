3d799eaeec468da90c3e9be6e565ad1c43e925d8
Aziiiz <asirojiddinov96@gmail.com>
2021-11-02 18:13:54 +0900

    test cases

--- tests/test_lockfutures_02.py
+++ tests/test_lockfutures_02.py
@@ -24,7 +24,7 @@ def test_lock_tokens(accounts, projecttoken, nulltoken, lockfutures):
     lockfutures.lockTokens(
         nulltoken.address,
         LOCKED_AMOUNT,
-        [chain.time() + 2000000, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
+        [chain.time() + 900001, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
          chain.time() + 500000000],
         [10e18, 30e18, 30e18, 15e18, 15e18],
         [accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]],
@@ -35,7 +35,7 @@ def test_lock_tokens(accounts, projecttoken, nulltoken, lockfutures):
     lockfutures.lockTokens(
         projecttoken.address,
         LOCKED_AMOUNT,
-        [chain.time() + 2000000, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
+        [chain.time() + 900002, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
          chain.time() + 500000000],
         [10e18, 30e18, 30e18, 15e18, 15e18],
         [accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]],
@@ -47,7 +47,7 @@ def test_lock_tokens(accounts, projecttoken, nulltoken, lockfutures):
     lockfutures.lockTokens(
         nulltoken.address,
         LOCKED_AMOUNT,
-        [chain.time() + 2000000, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
+        [chain.time() + 900003, chain.time() + 5000000, chain.time() + 32000000, chain.time() + 42000000,
          chain.time() + 500000000],
         [10e18, 30e18, 30e18, 15e18, 15e18],
         [accounts[1], accounts[2], accounts[3], accounts[4], accounts[5], accounts[6]],
@@ -62,28 +62,18 @@ def test_lock_tokens(accounts, projecttoken, nulltoken, lockfutures):
 
 def test_lockfutures_emit(accounts, projecttoken, nulltoken, lockfutures,erc1155):
 
-
-    # with reverts("Claiming NFT insufficient for now"):
-    #     lockfutures.claimWithNFT(0)
-
         logging.info(lockfutures.getLockCount())
-
-        lockfutures.emitFutures(0,0, {'from': accounts[3]})
-
-        with reverts("This futures already issued"):
-            lockfutures.emitFutures(0, 0, {'from': accounts[1]})
 
         logging.info(erc1155.balanceOf(accounts[1], 0))
 
         logging.info(lockfutures.getLockRecordByIndex(0))
-        lockfutures.emitFutures(0, 1, {'from': accounts[2]})
 
 
         logging.info(lockfutures.getLockRecordByIndex(0))
-
-        lockfutures.emitFutures(0, 2, {'from': accounts[3]})
-        logging.info(erc1155.balanceOf(accounts[1], 2))
-
+        for i in range(0, 4):
+            for j in range(0, 5):
+                lockfutures.emitFutures(i, j, {'from': accounts[3]})
+                logging.info(lockfutures.getNFTIdByLockVestingIndexes(i,j))
 
 
         ## claim
@@ -92,14 +82,31 @@ def test_lockfutures_emit(accounts, projecttoken, nulltoken, lockfutures,erc1155
 
         chain.mine(200)
 
-
         chain.sleep(90000)
 
-
         chain.mine(200)
-        nft_before_amount = erc1155.balanceOf(accounts[4], 0)
-        lockfutures.claimWithNFT(0, {'from': accounts[4]})
+        for k in range(1,6):
+            nft_before_amount = erc1155.balanceOf(accounts[k], 0)
+            lockfutures.claimWithNFT(0, {'from': accounts[k]})
+            assert erc1155.balanceOf(accounts[k], 0) == 0
+            assert projecttoken.balanceOf(accounts[k]) == nft_before_amount
+
+
+        for i in range(1,6):
+            with reverts("Your futures balance is zero"):
+                lockfutures.claimWithNFT(0, {'from': accounts[k]})
 
 
 
-        assert projecttoken.balanceOf(accounts[4]) == nft_before_amount
+        for i in range(1,4):
+            with reverts("Claiming NFT insufficient for now"):
+                lockfutures.claimWithNFT(i, {'from': accounts[3]})
+
+
+
+        for x in range(1,4):
+            for y in range(1,5):
+                logging.info(lockfutures.getNFTIdByLockVestingIndexes(x,y))
+                with reverts("Claiming NFT insufficient for now"):
+                    lockfutures.claimWithNFT(lockfutures.getNFTIdByLockVestingIndexes(x,y), {'from': accounts[3]})
+
